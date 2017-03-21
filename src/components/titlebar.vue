bb9a9137a9cef841dee05d80f845bcd5d8e6c9c5
liushengxian <liushengxian1230@163.com>
2017-03-22 02:22:54 +0800

    tmp push

---
+++ src/components/titlebar.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="banner">
+		<img src="http://placehold.it/18x18">
+		<h2>Hacker News</h2>
+		<span>new</span>
+		<span>comments</span>
+		<span>show</span>
+		<span>ask</span>
+		<span>jobs</span>
+	</div>
+</template>
+
+<script>
+	module.exports = {
+		
+	}
+</script>
